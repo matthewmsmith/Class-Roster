f6714578acd63c67c78f6add0ef633180a60b44a
matthewmsmith <matthewmsmith1992@gmail.com>
2021-08-30 12:00:13 -0400

    Initial Commit

---
+++ Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Code
+//
+//  Created by Matthew Smith on 8/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
